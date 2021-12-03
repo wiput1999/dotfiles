f5181dcdd38360c5d3d3a8f4ca32c8b4c18855ff
Wiput Pootong <wiput.pootong@gmail.com>
2021-12-03 09:59:57 +0700

    feat: macos config

--- chezmoi/private_dot_config/fish/conf.d/paths.fish
+++ chezmoi/private_dot_config/fish/conf.d/paths.fish
@@ -7,32 +7,33 @@ add_path /usr/local/opt/coreutils/libexec/gnubin
 
 add_path $GEM_HOME/bin
 
+add_path /opt/homebrew/bin
+
 add_path /usr/local/bin /usr/bin /usr/local/sbin /usr/libexec
 
-add_path $ANDROID_HOME/tools
-add_path $ANDROID_HOME/platform-tools
+# add_path $ANDROID_HOME/tools
+# add_path $ANDROID_HOME/platform-tools
 
-add_path $HOME/.cargo/bin
-add_path $HOME/lib/gcloud/bin
+# add_path $HOME/.cargo/bin
+# add_path $HOME/lib/gcloud/bin
 add_path $HOME/.config/yarn/global/node_modules/.bin
 
-add_path $HOME/.poetry/bin
-add_path $HOME/lib/flutter/bin
-add_path $HOME/lib/flutter/.pub-cache/bin
-add_path /usr/local/opt/dart@2/bin
+# add_path $HOME/.poetry/bin
+# add_path $HOME/lib/flutter/bin
+# add_path $HOME/lib/flutter/.pub-cache/bin
+# add_path /usr/local/opt/dart@2/bin
 
-add_path $GOPATH/bin
+# add_path $GOPATH/bin
 add_path $HOME/bin $HOME/.local/bin
 
-add_path $HOME/.platformio/penv/bin
+# add_path $HOME/.platformio/penv/bin
 
-add_path $HOME/lib/emsdk/emscripten/1.37.34
+# add_path $HOME/lib/emsdk/emscripten/1.37.34
 
-add_path $HOME/.composer/vendor/bin
+# add_path $HOME/.composer/vendor/bin
 
 add_path $HOME/.asdf/bin
 add_path $HOME/.asdf/shims
 
 # Use gnu manuals too
 set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman/ $MANPATH
-
