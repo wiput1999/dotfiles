a238feb2c6cd9cd8556c87de96659afe1a92f646
Wiput Pootong <wiput.pootong@gmail.com>
2021-12-04 01:31:42 +0700

    feat: fish

--- chezmoi/private_dot_config/fish/functions/asdf.fish
+++ chezmoi/private_dot_config/fish/functions/asdf.fish
@@ -1,9 +1,15 @@
-set -l asdf_data_dir (
-  if test -n "$ASDF_DATA_DIR"; echo $ASDF_DATA_DIR;
-  else; echo $HOME/.asdf; end)
+set -x ASDF_DIR (dirname (status -f))
+
+set -l asdf_user_shims (
+  if test -n "$ASDF_DATA_DIR"
+    echo $ASDF_DATA_DIR/shims
+  else
+    echo $HOME/.asdf/shims
+  end
+)
 
 # Add asdf to PATH
-set -l asdf_bin_dirs $ASDF_DIR/bin $ASDF_DIR/shims $asdf_data_dir/shims
+set -l asdf_bin_dirs $ASDF_DIR/bin $asdf_user_shims
 
 for x in $asdf_bin_dirs
     if test -d $x
@@ -17,11 +23,5 @@ for x in $asdf_bin_dirs
     set PATH $x $PATH
 end
 
-function asdf_update_java_home --on-event fish_prompt
-    set --local java_path (asdf which java)
-    if test -n "$java_path"
-        set --local full_path (realpath "$java_path")
-        set --local full_path_dir (dirname "$full_path")
-        set -gx JAVA_HOME (dirname "$full_path_dir")
-    end
-end
+# Load the asdf wrapper function
+source $ASDF_DIR/lib/asdf.fish
