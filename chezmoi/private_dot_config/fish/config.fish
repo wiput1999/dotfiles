b591958b5e7a6002d4e368b47ca97ce6feb80850
Wiput Pootong <wiput.pootong@gmail.com>
2022-01-12 15:40:31 +0700

    feat: add asdf

--- chezmoi/private_dot_config/fish/config.fish
+++ chezmoi/private_dot_config/fish/config.fish
@@ -30,5 +30,7 @@ set -g fish_user_paths /usr/local/opt/gettext/bin $fish_user_paths
 
 # thefuck --alias | source
 
+source ~/.asdf/asdf.fish
+
 set -g fish_user_paths /usr/local/opt/icu4c/bin $fish_user_paths
 set -g fish_user_paths /usr/local/opt/icu4c/sbin $fish_user_paths
