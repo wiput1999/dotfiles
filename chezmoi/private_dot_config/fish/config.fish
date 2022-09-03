d5c77ed051f62024825e6dbfdf1062aca39cbba4
Wiput Pootong <wiput.pootong@gmail.com>
2022-09-04 03:31:18 +0700

    fix: PNPM_HOME variable

--- chezmoi/private_dot_config/fish/config.fish
+++ chezmoi/private_dot_config/fish/config.fish
@@ -1,24 +1,6 @@
 # Enable vi key bindings
 fish_vi_key_bindings
 
-# Python Environment Manager
-# pyenv init - | source
-# pyenv virtualenv-init - | source
-
-# Golang Environment Manager
-# goenv init - | source
-
-# jenv init - | source
-
-# Google Cloud
-# if test ~/lib/gcloud/path.fish.inc
-#     source ~/lib/gcloud/path.fish.inc
-# end
-
-# function virtualfish
-#     eval (python3 -m virtualfish)
-# end
-
 if type -q kitty
     kitty + complete setup fish | source
 end
@@ -31,6 +13,8 @@ set -g fish_user_paths /usr/local/opt/gettext/bin $fish_user_paths
 # thefuck --alias | source
 
 source ~/.asdf/asdf.fish
+
+set -gx PNPM_HOME "$HOME/Library/pnpm"
 
 set -g fish_user_paths /usr/local/opt/icu4c/bin $fish_user_paths
 set -g fish_user_paths /usr/local/opt/icu4c/sbin $fish_user_paths
